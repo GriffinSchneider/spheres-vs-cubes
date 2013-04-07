2a8019cbb16deada5d3c327af992834dde5a1011
Griffin Schneider <griffinschneider@gmail.com>
2013-04-06 23:23:42 -0400

    Fix rotation. I have no clue what a quaternion is, but apparently that's what bullet uses for rotation.

--- src/Objects/PObject.java
+++ src/Objects/PObject.java
@@ -66,19 +66,37 @@ public abstract class PObject {
 		body.setUserPointer(this);
 	}
 	
+	// From http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToAngle/
+	public void rotateFromQuaternion(Quat4f q1) {
+		float x, y, z, angle;
+		if (q1.w > 1) q1.normalize(); // if w>1 acos and sqrt will produce errors, this cant happen if quaternion is normalised
+		angle = (float) (2 * Math.acos(q1.w));
+		float s = (float) Math.sqrt(1-q1.w*q1.w); // assuming quaternion normalised then w is less than 1, so term always positive.
+		if (s < 0.001) { // test to avoid divide by zero, s is always positive due to sqrt
+			// if s close to zero then direction of axis not important
+			x = q1.x; // if it is important that axis is normalised then replace with x=1; y=z=0;
+			y = q1.y;
+			z = q1.z;
+		} else {
+			x = q1.x / s; // normalise axis
+			y = q1.y / s;
+			z = q1.z / s;
+		}
+		applet.rotate(-angle, x, y, z);
+	}
+	
 	public void visit() {
 		body.activate();
 		update();
 		
-		applet.pushMatrix();
 		body.getMotionState().getWorldTransform(trans);
+		
+		applet.pushMatrix();
 		applet.translate(
 				trans.origin.x * GRAPHICS_UNITS_PER_PHYSICS_UNITS, 
 				-trans.origin.y * GRAPHICS_UNITS_PER_PHYSICS_UNITS,
-				trans.origin.z * GRAPHICS_UNITS_PER_PHYSICS_UNITS);
-		Quat4f r = trans.getRotation(new Quat4f());
-		applet.rotate(r.w, r.x, r.y, r.z);
-		
+				trans.origin.z * GRAPHICS_UNITS_PER_PHYSICS_UNITS);		
+		rotateFromQuaternion(trans.getRotation(new Quat4f()));
 		draw();
 		applet.popMatrix();
 	}
